7cc7233ee0b791318c60259f25453239fae6383b
Bekzat Shayakhmetov <smbekzat@hotmail.com>
2017-05-27 12:55:22 +0700

    static root2

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
